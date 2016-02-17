47caf11dd72bc7273b78d3a7aaa8d09904f9f0fd
Dorian Baies <Dorian Baies>
2016-02-17 11:19:21 +0100

    Slider is nog steeds kut

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>php opdrachten</title>
+  <title>Project</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -9,7 +9,7 @@
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="style.css">
   <?php require 'navbar.php'; 
-        include 'deep-minified.html';
+        require 'slider.html';
   ?>
  
 </head>
--- navbar.php
+++ navbar.php
@@ -8,22 +8,22 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="main.php">WebSiteName</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
       <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown">oefeningen
+        <a class="dropdown-toggle" data-toggle="dropdown">Oefeningen
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">Rekenen</a></li>
           <li><a href="#">Nederlands</a></li>
           <li><a href="#">Engels</a></li>
-          <li><a href="#">Biology</a></li> 
+          <li><a href="#">Biologie</a></li> 
         </ul>
       </li>
-      <li><a href="#">over ons</a></li> 
-      <li><a href="#">contact</a></li> 
+      <li><a href="#">Over ons</a></li> 
+      <li><a href="#">Contact</a></li> 
     </ul>
   </div>
 </nav>
