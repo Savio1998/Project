ae24e9759f21a21fd95bc024668f06896501bb00
sjoerd akkerman <sakkerman@roc-dev.com>
2016-02-17 09:49:15 +0100

    hoofdpagina

---
+++ navbar.php
@@ -0,0 +1,29 @@
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown">oefeningen
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Rekenen</a></li>
+          <li><a href="#">Nederlands</a></li>
+          <li><a href="#">Engels</a></li>
+          <li><a href="#">Biology</a></li> 
+        </ul>
+      </li>
+      <li><a href="#">over ons</a></li> 
+      <li><a href="#">contact</a></li> 
+    </ul>
+  </div>
+</nav>
