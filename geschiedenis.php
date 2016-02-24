e31866ebc52f258875f97353a719ff44116ce5be
donnysmit <donnysmit@github.com>
2016-02-24 15:39:43 +0100

    geschiedenis

---
+++ geschiedenis.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Geschiedenis</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <?php require 'navbar.php'; ?>
+ 
+</head>
+<body style="text-align:center;"> 
+<h3><b>Geschiedenis</b></h3><br>
+<h4><b>In welk jaar begon de eerste wereldoorlog?</b></h4><br>
+<?php 
+    $gevraag= array
