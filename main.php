6cb1891b96cf8a16bc14e30561b8ea84ee316afa
klinten kwasiba <klinten kwasiba>
2016-02-24 11:24:32 +0100

    main drop gefixed + engelse les

--- main.php
+++ main.php
@@ -2,20 +2,16 @@
 <html>
 <head>  
     <title>Project</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <?php require 'navbar.php';
+        require 'slider.html'; ?>
 </head>
 <body style="padding:0px; margin:0px; background-color:#fff;font-family:Arial, sans-serif">
-    
-    <?php 
-        require 'navbar.php'; 
-        require 'slider.html';
-    ?>
     
     <h1 style = "text-align:center; font-family: 'Coming Soon', cursive; text-align: center; font-size: 40px; text-color:#fff; text-shadow: 2px 2px #f0f0f0">Welkom bij De Ooievaar</h1>
 </body>
