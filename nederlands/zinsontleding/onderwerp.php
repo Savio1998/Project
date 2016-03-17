fd324e40fc45970a0f936d9a576bc170c312944c
YumNoodleNick <nickmensink168@hotmail.com>
2016-03-17 09:38:14 +0100

    Nederlands en mapjes

---
+++ nederlands/zinsontleding/onderwerp.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Onderwerp</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <?php require 'navbar.php'; ?>
+ 
+</head>
+<body>    
+<?php
+?>
+
+<div class="container">
+<center><h3><b>Onderwerp</b></h3></center>
+<br>
+<h4><b>Het onderwerp van een tekst is altijd de persoon die een handeling uitvoert. 
+               Als ik zeg: "Ik ga lopen" dan is "Ik" het onderwerp. 
+               Dit komt omdat "Ik" op dat moment de handeling "gaan" verricht.
+               "ga" is in dit geval de persoonsvorm.
+               Een eenvoudige manier om achter het onderwerp te komen is door de vraag: "Wie [persoonsvorm]?".
+               Hierin moet je zelf de persoonsvorm plaatsen en vervolgens de vraag beantwoorden.
+               Het antwoord op die vraag is dan het onderwerp.
+               Bijvoorbeeld bij de zin "Ik ga lopen" kan je de vraag "Wie gaat?".
+               In dit geval is het antwoord dus "Ik".
+               Hieronder vind je een aantal knoppen die je naar een pagina leiden met wat oefeningen over het onderwerp.</b></h4>
+<br>
+<br>
+<div class="row">
+<div class="col-md-4"><center><a href="onderwerp1.php"><img src="project/niveau1.png" alt="" style="width:300px;height:300px;"></a><center></div>
+<div class="col-md-4"><center><a href="onderwerp2.php"><img src="niveau2.png" alt="" style="width:300px;height:300px;"></a><center></div>
+<div class="col-md-4"><center><a href="onderwerp3.php"><img src="niveau3.png" alt="" style="width:300px;height:300px;"></a><center></div>
+</div>
+
+        
+        
+    </div>      
+</body>
+</html>
---
+++ nederlands/zinsontleding/onderwerp1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Onderwerp niveau 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <?php require 'navbar.php'; ?>
+ 
+</head>
+<body>    
+<?php
+?>
+
+<div class="container">
+<center><h3><b>Niveau 1</b></h3></center>
+<br>
+<br>
+<div class="row">
+<div class="col-md-4"><b>Oefeningen</b></div>
+<div class="col-md-4"></div>
+<div class="col-md-4"><b>Score</b></div>
+</div>
+<br>
+<div class="row">
+<div class="col-md-4"><a href="onderwerp1.1.php">Onderwerp oefening 1</a></div>
+<div class="col-md-4"></div>
+<div class="col-md-4"></div>
+</div>
+</div>        
+        
+         
+</body>
+</html>
