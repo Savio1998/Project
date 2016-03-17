7fd3b78d1687703b03ba72ceadbd87598916576a
YumNoodleNick <nickmensink168@hotmail.com>
2016-03-17 10:40:44 +0100

    help3

---
+++ opdrachten/geschiedenis/geschiedenis.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<title>Geschiedenis</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php require 'navbar.php'; ?>
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h1>Vul de juiste vakjes in</h1>
+		
+		<form action="geschiedenisscore.php" method="get" id="quiz">
+		
+            <ol>
+            
+                <li>
+                
+                    <h3>Hoe heette het Japanse eiland dat gebombardeerd werd door de Verenigde Staten?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) Hiyoshi. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) Hiroshima. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) Hiroshige. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) Hirondelle. </label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Wat was de naam van de tijd waar het goed ging met Nederland in de 17e eeuw? </h3>
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) De rustige tijd. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) Tijd van goud. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) De goude eeuw. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                        <label for="question-2-answers-D">D) Het magische tijdperk. </label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Waar staat de afkorting VOC voor?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) Verenigde Oost-Indische Compagnie. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) Verenigde onderhandel compagnie. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) Verplaatsing onder communicatie. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                        <label for="question-3-answers-D">D) Verenigde organisatische connectie. </label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Welk feit hieronder is waar?</h3>
+                   
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) De rijke kwamen bijna altijd op voor de arme. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) Er was een groot verschil tussen arm en rijk. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) De stadhouder kwam op voor de arme. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                        <label for="question-4-answers-D">D) Veel startende vakbonden haalde het niet. </label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Welk feit is niet waar?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) Water voor de arme was vies en ongezond. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) De goude eeuw duurde tot 1702</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) Nederland werd bestuurd door een buitenlandse stadhouder. </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                        <label for="question-5-answers-D">D) Ook vrouwen werkten. </label>
+                    </div>
+                
+                </li>
+            
+            </ol>
+            
+            <input type="submit" value="Submit Quiz" name="submit" />
+		
+		</form>
+	
+	</div>	
+</body>
+</html>
