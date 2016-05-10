613b568fcb0087fb9e780a8558bd8ee592fb7847
learn2draw <learn2draw@live.nl>
2016-05-10 08:49:31 +0200

    Update opdrachten.php

--- opdrachten.php
+++ opdrachten.php
@@ -59,11 +59,8 @@
 			<a href="/opdrachten/engels.php">
 				<img src="afbeeldingen/blackboard1_09.gif" width="659" height="210" border="0" alt=""></a></td>
 		<td colspan="2">
-<<<<<<< HEAD
 			<a href="/opdrachten/topografie.php">
-=======
 			<a href="/opdrachten/topografie.php">
->>>>>>> ccfc2fd0bc6d8d4127231810c6c4a3d57d632c5a
 				<img src="afbeeldingen/blackboard1_10.gif" width="683" height="210" border="0" alt=""></a></td>
 		<td rowspan="5">
 			<img src="afbeeldingen/blackboard1_11.gif" width="223" height="604" alt=""></td>
