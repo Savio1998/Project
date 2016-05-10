6d2ffd64bf4e47de7093a32776c24286e9056418
klinten kwasiba <klinten kwasiba>
2016-05-10 07:48:19 +0200

    Engels met styling -L3

---
+++ opdrachten/engels/readingexercise/level3/readenglish1.php
@@ -0,0 +1,68 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>English reading n3</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="level1/text.css">
+  <?php require '../../../../navbar.php'; ?>
+	<title>English reading n3</title>
+</head>
+ <style>
+    div.reading {
+    border: 2px solid black;
+    padding: 50px;
+    padding-left: 120px;
+    padding-right: 120px;
+    text-align: justify;
+    background-color: whitesmoke;
+    line-height: 1.5;
+    }
+    </style>
+</head>
+<body>
+    <div class="container"> 
+        <div class="row">
+                <div class="reading"><h4>
+                  RAF Upper Heyford - once the heart of allied defence against nuclear attack by the USSR - could become a Cold War 'museum'. 
+
+Historians want parts of the base to be preserved as a heritage centre that could show future generations the struggle with Soviet communism 'in a way no document can'.
+
+Details of the latest recommendations for Heyford - now being called Heyford Park - have been put forward by English Heritage which has called for measures to prevent demolition of the 'irreplaceable' military remains.
+
+Current thinking comes from a detailed assessment of Cold War infrastructure across England by English Heritage experts. Keith Watson, the chief executive of the North Oxfordshire Consortium who are to develop part of the site for housing, said they were in full agreement with English Heritage's proposals.<br>
+
+<br>He said: "We are quite content with what English Heritage is proposing. It has always been part of our scheme to retain these structures in any event. "We are working with English Heritage to agree a consistent plan for the buildings."
+
+David Went, English Heritage inspector of ancient monuments, said many Upper Heyford features exemplify historical aspects of national importance about the Cold War.<br>
+
+"The sheer scale and bare functionality of the structures on the base can illustrate for present and future generations, in a way no document can, the reality of the struggle with Soviet Communism," he said.<br>
+
+"In our view much of this character would be lost by future ill-thought-out change and there stands an opportunity to ensure this does not happen.
+
+"We recognize that preservation of the whole base exactly as it stands today may not be a realistic option but a sustainable future could be found which balances the need for preservation against other needs."<br>
+
+<br>Mr Went said the English Heritage view was that the future appearance of the base should include the most significant monuments and should:<br>
+
+<br>keep the open character of the runway area without planting schemes planned by developers<br>
+keep a section of the main runway and the remainder as a grassed avenue<br>
+provide all-weather access to the monuments, preferably by keeping existing base taxiways and perimeter tracks, for visitors or other practical use<br>
+preserve the present landscape balance around the bomb bunkers and quick reaction area.<br><br>
+The English Heritage study, submmitted to the Planning Inspectorate in advance of the public inquiry into planning wrangles over the base which started at Bodicote House yesterday, has revealed that much of the Heyford landscape prior to becoming an airbase was open common or heathland a feature <br> Cherwell District Council planners would like re-established as a local country park.<br>
+
+The council aims to defend the accepted 1,000-home plan which the North Oxfordshire Consortium of developers wishes to extend to over 5,000 homes.</h4>
+
+                <form action="readquestions1.php" method="get" id="questions">
+                    <input type="submit" value="Next" name="Next" />
+                    </div>
+                </form>
+            </div>
+        </div>  
+    </div>     
+</body>
+</html>
