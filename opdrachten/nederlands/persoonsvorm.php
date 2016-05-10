38445f5a839f67d0e2e8650a08ddbec368f80231
YumNoodleNick <nickmensink168@hotmail.com>
2016-05-10 10:15:35 +0200

    oops

--- opdrachten/nederlands/persoonsvorm.php
+++ opdrachten/nederlands/persoonsvorm.php
@@ -36,11 +36,11 @@ De persoonsvorm van een zin is een handeling die wordt uitgevoert.
 </div>
 <br>
 <div class="row">
-<div class="col-md-4"><?php if($_COOKIE['opdr-nl-pers-opdr1'] == 0) { ?><a href="./opdrachten1.php">Persoonsvorm Oefeningen 1</a><?php }else{ ?> Persoonsvorm Oefeningen 1 <?php } ?></div>
+<div class="col-md-4"><?php if($_COOKIE['opdr-nl-pers-opdr1'] == 0) { ?><a href="./persoonsvorm/opdrachten1.php">Persoonsvorm Oefeningen 1</a><?php }else{ ?> Persoonsvorm Oefeningen 1 <?php } ?></div>
 <div class="col-md-4"></div>
 <div class="col-md-4"> <?=isset($_COOKIE["opdr-nl-pers-opdr1"]) ? $_COOKIE['opdr-nl-pers-opdr1'] : 0 ?>/100</div><br>
 
-<div class="col-md-4"><?php if($_COOKIE['opdr-nl-pers-opdr2'] == 0) { ?><a href="./opdrachten2.php">Persoonsvorm Oefeningen 2</a><?php }else{ ?> Persoonsvorm Oefeningen 2 <?php } ?></div>
+<div class="col-md-4"><?php if($_COOKIE['opdr-nl-pers-opdr2'] == 0) { ?><a href="./persoonsvorm/opdrachten2.php">Persoonsvorm Oefeningen 2</a><?php }else{ ?> Persoonsvorm Oefeningen 2 <?php } ?></div>
 <div class="col-md-4"></div>
 <div class="col-md-4"> <?=isset($_COOKIE["opdr-nl-pers-opdr2"]) ? $_COOKIE['opdr-nl-pers-opdr2'] : 0 ?>/100</div><br>
 </div>
